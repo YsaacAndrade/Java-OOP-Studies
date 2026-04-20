86779047330b08cf815ab587a8f7edb963a482bb
Ysaac Andrade <ysaacandrade07@gmail.com>
2026-04-20 20:55:30 -0300

    JavaDBC

---
+++ JavaConnectDB/src/MyJDBC.java
@@ -0,0 +1,20 @@
+import java.sql.*;
+
+public class MyJDBC {
+    public static void main(String[] args) {
+        try{
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://127.0.0.1:3306/login_schema", "root", "hero123AD_");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM USERS");
+
+            while(resultSet.next()) {
+                System.out.println(resultSet.getString("username"));
+                System.out.println(resultSet.getString("password"));
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
